8db1bc33f3e8231cac04989d7325b6ef8307a880
Matheus Castro <13924985+matheuscarius@users.noreply.github.com>
2019-11-27 17:27:45 -0300

    Add file reading helpers (#4)

---
+++ include/files.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include<string>
+#include<vector>
+#include<opencv2/core.hpp>
+
+std::vector<cv::Mat> readImagesInFolder(const std::string& path); 
+std::vector<std::string> getFilePathsInFolder(const std::string& path);
---
+++ src/files.cpp
@@ -0,0 +1,23 @@
+#include "files.hpp"
+#include <filesystem>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+vector<Mat> readImagesInFolder(const string& path) {
+    const vector<string> file_paths = getFilePathsInFolder(path);
+    vector<Mat> I(file_paths.size());
+    for(int i = 0; i < file_paths.size(); i++) {
+        I[i] = imread(file_paths[i], IMREAD_GRAYSCALE);
+    }
+    return I;
+}
+
+vector<string> getFilePathsInFolder(const string& path) {
+  vector<string> r;
+  for (const auto& entry : filesystem::directory_iterator(path))
+    if (entry.is_regular_file()) r.push_back(entry.path());
+  return r;
+}
