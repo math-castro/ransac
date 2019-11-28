019e071fc4b8659fc6159d9c106b6c66b449929b
Matheus Castro <13924985+matheuscarius@users.noreply.github.com>
2019-11-28 18:38:05 +0100

    Implements RANSAC

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# project-inf573
-RANSAC Implementation in C++
+# Generic RANSAC in C++ and Image Stitching
+Implementation of a generic RANSAC (Random sample consensus) in C++ and of Image Stitching (Panorama) of multiple images (mosaic stitching).
+
+## Dependencies
+- CMake
+- OpenCV
+- C++17
--- include/ransac.hpp
+++ include/ransac.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include<vector>
+#include<random>
+#include<algorithm>
+#include<exception>
 
 /*
 *ComputeModel: should derive from BaseComputeModel with the same Model and Measurement
@@ -12,13 +15,36 @@
 template <class Model, class Measurement, class ComputeModel,
           class ComputeError>
 Model ransac(const std::vector<Measurement>& measurements, int n, int k, double t) {
-  // TODO: implement RANSAC
+  using namespace std;
+
+  if(n > measurements.size()) {
+    throw invalid_argument("The number of measurements cannot be smaller than n");
+  }
+
   ComputeModel compute_model;
   ComputeError compute_error;
+  mt19937 rng(random_device{}());
 
-  Model model = compute_model(measurements);
-  Measurement measurement;
+  Model best_model;
+  int n_inliers=0;
 
-  compute_error(model, measurement);
-  return model;
+  for(int i = 0; i < k; i++) {
+    // create a sampling
+    vector<Measurement> sampling;
+    sample(measurements.begin(), measurements.end(), back_inserter(sampling), n, rng);
+    // compute model
+    Model model = compute_model(sampling);
+    // compute number of inliers
+    int cur_inliers = 0;
+    for(const Measurement &measurement : measurements)
+      if(compute_error(model, measurement) < t)
+        cur_inliers++;
+    // if has more inliers, set as best model
+    if(cur_inliers > n_inliers) {
+      best_model = model;
+      n_inliers = cur_inliers;
+    }
+  }
+
+  return best_model;
 }
